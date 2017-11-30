294cad6009d381dde8648bfc50b83e525448e7a0
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-11-30 19:21:57 +0530

    test cases fixes

--- src/app/trends/services/youtube.service.spec.ts
+++ src/app/trends/services/youtube.service.spec.ts
@@ -1,12 +1,15 @@
+import { Http } from '@angular/http';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { YoutubeService } from './youtube.service';
+import { SharedModule } from 'app/shared/shared.module';
 
 describe('YoutubeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [YoutubeService]
+      providers: [YoutubeService],
+      imports: [SharedModule]
     });
   });
 
